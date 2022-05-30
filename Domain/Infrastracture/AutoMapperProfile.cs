58df4803f6facd3ab220773cc4637be8881c5048
Bohdan Papirniak <bohdan_papirniak@epam.com>
2022-05-30 10:38:27 +0300

    Adding Controller, DTO, AutoMApper,

---
+++ Domain/Infrastracture/AutoMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Domain.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Infrastracture
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Product,ProductDto>().ReverseMap();
+        }
+    }
+}
